f1d3ca375bf4f286b69bf03e12a24fcf3087b8b2
Eloiza <eloiza.souza@zup.com.br>
2024-12-02 15:40:01 -0300

    feat: create project

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Eloiza";
+        int age = 44;
+        double height = 1.59;
+        boolean isRegistered = true;
+
+        System.out.println("Nome: " + name + " Idade: " + age + " Altura: " + height + " Matriculado: " + true);
+
+    }
+}
